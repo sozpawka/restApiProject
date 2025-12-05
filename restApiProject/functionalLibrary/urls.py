6371ea3fb8072fed08f430d09f453511fb9466d4
Sozpawka <sozpawkawar@gmail.com>
2025-12-05 14:50:49 +0700

    2_commit_05.12.2025

---
+++ restApiProject/functionalLibrary/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthorViewSet
+
+router = DefaultRouter()
+router.register("authors", AuthorViewSet, basename="author")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- restApiProject/functionalLibrary/views.py
+++ restApiProject/functionalLibrary/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Author
+from .serializers import AuthorSerializer
 
-# Create your views here.
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [AllowAny]
